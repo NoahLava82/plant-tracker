e220f5b8fb436e03e065431de3a7dc283fc463cb
Noah Lavayen <noah.lavayen2@gmail.com>
2025-11-05 14:41:45 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // make sure this matches your src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class", // optional for dark mode
+};
